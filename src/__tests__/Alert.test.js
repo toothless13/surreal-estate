b7d61a518b67463bb1c300df288ce7e6a5abc2c5
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-19 16:01:00 +0100

    add error and success tests for Alert component and write passing code

---
+++ src/__tests__/Alert.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+// import { MemoryRouter } from "react-router-dom";
+import Alert from "../components/Alert";
+
+describe("Alert", () => {
+  it("renders an error message", () => {
+    const { getByText } = render(<Alert message="Error!" />);
+
+    expect(getByText(/Error/).textContent).toBe("Error!");
+  });
+
+  it("renders a success message", () => {
+    const { getByText } = render(<Alert message="Success!!!" />);
+
+    expect(getByText(/Success!!!/).textContent).toBe("Success!!!");
+  })
+});
