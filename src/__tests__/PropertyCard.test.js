3a49d8358b309c0266e5a2febe67afa0d121f7ad
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-20 15:56:13 +0100

    start writing PropertyCard tests

---
+++ src/__tests__/PropertyCard.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import PropertyCard from "../components/PropertyCard";
+
+describe("PropertyCard", () => {
+  const validProps = {
+    title: "Test Title",
+    type: "Test Type",
+    bedrooms: 3,
+    bathrooms: 1,
+    price: 12345,
+    city: "Test City",
+    email: "test@email.com",
+  };
+
+  
+});
