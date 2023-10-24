ad5b9808176cf55c0c17dc59a61bb16319b46565
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-24 15:43:06 +0100

    add FontAwesome to project and PropertyCard component

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBed, faBathtub } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const PropertyCard = ({
   title,
@@ -14,14 +17,17 @@ const PropertyCard = ({
       <p className="property-card-title">{title}</p>
       <p className="property-card-type">{type}</p>
       <p className="property-card-bedrooms" data-testid="property-bedrooms">
+        <FontAwesomeIcon icon={faBed} />
         {bedrooms}
       </p>
       <p className="property-card-bathrooms" data-testid="property-bathrooms">
+        <FontAwesomeIcon icon={faBathtub} />
         {bathrooms}
       </p>
-      <p className="property-card-price">{price}</p>
+      <p className="property-card-price">£{price}</p>
       <p className="property-card-city">{city}</p>
       <a href={`mailto:${email}`} className="property-card-email">
+        <FontAwesomeIcon icon={faEnvelope} />
         Email
       </a>
     </div>
