52c2ea7a14cad6e42f095bb35bc8ebcd019b4648
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-26 20:19:41 +0100

    add tests for SideBar component

---
+++ src/__tests__/SideBar.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import SideBar from "../components/SideBar";
+
+describe("SideBar", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <MemoryRouter>
+        <SideBar />
+      </MemoryRouter>,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(screen.getByText(/Filter By City/i)).toBeInTheDocument();
+    expect(screen.getByText(/Manchester/i)).toBeInTheDocument();
+    expect(screen.getByText(/Leeds/i)).toBeInTheDocument();
+    expect(screen.getByText(/Oxford/i)).toBeInTheDocument();
+    expect(screen.getByText(/Birmingham/i)).toBeInTheDocument();
+    expect(screen.getByText(/Clear Filter/i)).toBeInTheDocument();
+  });
+});
